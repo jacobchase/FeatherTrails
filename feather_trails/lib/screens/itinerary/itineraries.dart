0c0e6576c497eac912063cdcce16773c8cad33dd
cfore21 <cfore21@lsu.edu>
2023-11-20 15:23:42 -0600

    Some changes made in order to try merging again

--- feather_trails/lib/screens/itinerary/itineraries.dart
+++ feather_trails/lib/screens/itinerary/itineraries.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'itin_cover.dart';
-import 'itin_item.dart';
-import 'itin_details.dart';
 
 class PlaceItineraryPage extends StatelessWidget {
-  final List<ItinCover> itinList;
 
-  PlaceItineraryPage({required this.itinList});
 
   @override
   Widget build(BuildContext context) {
@@ -34,25 +29,7 @@ class PlaceItineraryPage extends StatelessWidget {
 
         ]
     ),
-      body: ListView.builder(
-        itemCount: itinList.length,
-        itemBuilder: (context, index) {
-          ItinCover itinerary = itinList[index];
-          return ListTile(
-            title: Text(itinerary.itinTitle),
-            subtitle: Text('Start: ${itinerary.startPlace} | End: ${itinerary.endPlace}'),
-            onTap: () {
-              // Navigate to a detailed view of the itinerary when tapped
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ItinDetails(itin: itinerary),
-                ),
-              );
-            },
-          );
-        },
-      ),
+
     );
   }
 }
