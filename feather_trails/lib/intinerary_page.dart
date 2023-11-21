d5dcdf9f600dbd8819a5aecc2bccb9f43d2fac97
acrowe1 <113642049+acrowe1@users.noreply.github.com>
2023-11-21 00:02:01 -0600

    Update intinerary_page.dart

--- feather_trails/lib/intinerary_page.dart
+++ feather_trails/lib/intinerary_page.dart
@@ -2,99 +2,87 @@ import 'package:flutter/material.dart';
 import 'package:feather_trails/navBar.dart' as my_nav_bar;
 
 class ItineraryPage extends StatefulWidget {
-  final PageStorageBucket bucket;
-
-  const ItineraryPage({required this.bucket});
   @override
   ItineraryPageState createState() => ItineraryPageState();
 }
 
-class ItineraryPageState extends State<ItineraryPage>
-    with AutomaticKeepAliveClientMixin {
+class ItineraryPageState extends State<ItineraryPage> {
   int currentPageIndex = 1;
 
   @override
-  bool get wantKeepAlive => true;
-
   @override
   Widget build(BuildContext context) {
-    super.build(context);
-    return PageStorage(
-      bucket: widget.bucket,
-      key: const PageStorageKey('IntineraryPage'),
-      child: Scaffold(
-        body: Center(
-          child: Container(
-            alignment: Alignment.center,
-            child: Column(
-              children: [
-                const Text(''),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/NewItinerary');
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFFC93C13)),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white),
-                      minimumSize:
-                          MaterialStateProperty.all(const Size(360, 48)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
+    return Scaffold(
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              const Text(''),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/NewItinerary');
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFFC93C13)),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    minimumSize: MaterialStateProperty.all(const Size(360, 48)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
                     ),
-                    child: const Text('New Itinerary +'),
                   ),
+                  child: const Text('New Itinerary +'),
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/placeItinerary');
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          const Color(0xFFC93C13)),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white),
-                      minimumSize: MaterialStateProperty.all(const Size(
-                          double.infinity, 130)), // Adjust the size here
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/placeItinerary');
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFFC93C13)),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    minimumSize: MaterialStateProperty.all(const Size(
+                        double.infinity, 130)), // Adjust the size here
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
                       ),
                     ),
-                    child: const Text('Itinerary 1'),
                   ),
+                  child: const Text('Itinerary 1'),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-        bottomNavigationBar: my_nav_bar.NavigationBar(
-          selectedIndex: currentPageIndex,
-          onDestinationSelected: (int index) {
-            switch (index) {
-              case 0:
-                Navigator.pushNamed(context, '/');
-                break;
-              case 1:
-                break;
-              case 2:
-                Navigator.pushNamed(context, '/socialPage');
-                break;
-              case 3:
-                Navigator.pushNamed(context, '/profilePage');
-                break;
-            }
-          },
-        ),
+      ),
+      bottomNavigationBar: my_nav_bar.NavigationBar(
+        selectedIndex: currentPageIndex,
+        onDestinationSelected: (int index) {
+          switch (index) {
+            case 0:
+              Navigator.pushNamed(context, '/');
+              break;
+            case 1:
+              break;
+            case 2:
+              Navigator.pushNamed(context, '/socialPage');
+              break;
+            case 3:
+              Navigator.pushNamed(context, '/profilePage');
+              break;
+          }
+        },
       ),
     );
   }
