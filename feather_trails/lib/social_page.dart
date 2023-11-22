5cb9c4fabf7c384fe299bbc24f4ec23aa76c4e8c
acrowe1 <113642049+acrowe1@users.noreply.github.com>
2023-11-21 21:47:10 -0600

    Update social_page.dart - just the post buttons

--- feather_trails/lib/social_page.dart
+++ feather_trails/lib/social_page.dart
@@ -2,53 +2,124 @@ import 'package:flutter/material.dart';
 import 'package:feather_trails/navBar.dart' as my_nav_bar;
 
 class SocialPage extends StatefulWidget {
-  final PageStorageBucket bucket;
-
-  const SocialPage({required this.bucket});
   @override
   SocialPageState createState() => SocialPageState();
 }
 
-class SocialPageState extends State<SocialPage>
-    with AutomaticKeepAliveClientMixin {
-  @override
-  bool get wantKeepAlive => true;
-
+class SocialPageState extends State<SocialPage> {
   @override
   Widget build(BuildContext context) {
-    super.build(context);
-    return PageStorage(
-      bucket: widget.bucket,
-      key: const PageStorageKey('SocialPage'),
-      child: Scaffold(
-        body: Container(
-          alignment: Alignment.center,
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Welcome to the Social Page!'),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Be Social!"),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/socials1');
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Color(0xFFC93C13)),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  minimumSize: MaterialStateProperty.all(
+                      const Size(double.infinity, 150)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                child: const Stack(
+                  alignment: Alignment.centerLeft,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Smith Family Vacation',
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/socials2');
+                },
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Color(0xFFC93C13)),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  minimumSize: MaterialStateProperty.all(
+                      const Size(double.infinity, 150)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                child: const Stack(
+                  alignment: Alignment.centerLeft,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Icon(
+                        Icons.arrow_forward,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Nashville Girls' Trip",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
-        bottomNavigationBar: my_nav_bar.NavigationBar(
-          selectedIndex: 2, // Set the appropriate index for the SocialPage
-          onDestinationSelected: (int index) {
-            switch (index) {
-              case 0:
-                Navigator.pushNamed(context, '/');
-                break;
-              case 1:
-                Navigator.pushNamed(context, '/itineraryPage');
-                break;
-              case 2:
-                // Do something for index 2 if needed (already on SocialPage)
-                break;
-              case 3:
-                Navigator.pushNamed(context, '/profilePage');
-                break;
+      ),
+      bottomNavigationBar: my_nav_bar.NavigationBar(
+        selectedIndex: 2,
+        onDestinationSelected: (int index) {
+          setState(() {
+            if (index == 0) {
+              Navigator.pushNamed(context, '/');
             }
-          },
-        ),
+            if (index == 1) {
+              Navigator.pushNamed(context, '/itineraryPage');
+            }
+            if (index == 2) {
+              Navigator.pushNamed(context, '/socialPage');
+            }
+            if (index == 3) {
+              Navigator.pushNamed(context, '/profilePage');
+            }
+          });
+        },
       ),
     );
   }
