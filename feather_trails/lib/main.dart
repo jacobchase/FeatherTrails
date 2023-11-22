fca21fc28f8c4c1ba0a2d1bb8237ccbfaf8c19e5
acrowe1 <113642049+acrowe1@users.noreply.github.com>
2023-11-21 21:45:54 -0600

    Update main.dart - added Social pages routes

--- feather_trails/lib/main.dart
+++ feather_trails/lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:feather_trails/account_page.dart';
+import 'package:feather_trails/notification_page.dart';
 import 'package:feather_trails/privacy_policy.dart';
+import 'package:feather_trails/settings_page.dart';
+import 'package:feather_trails/shared_itineraries.dart';
 import 'package:feather_trails/navBar.dart' as my_nav_bar;
 import 'package:feather_trails/place_itinerary_page.dart';
 import 'package:feather_trails/intinerary_page.dart';
@@ -10,16 +14,16 @@ import 'package:feather_trails/forgot_password.dart';
 import 'package:feather_trails/sign_up.dart';
 import 'package:feather_trails/new_itinerary.dart';
 import 'package:feather_trails/map_screen.dart';
-import 'package:feather_trails/notification_page.dart';
-import 'package:feather_trails/settings_page.dart';
-import 'package:feather_trails/shared_itineraries.dart';
-import 'package:feather_trails/account_page.dart';
+import 'package:feather_trails/socials_s1.dart';
+import 'package:feather_trails/socials_s2.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final PageStorageBucket bucket = PageStorageBucket();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,12 +35,12 @@ class MyApp extends StatelessWidget {
           background: const Color.fromARGB(255, 255, 255, 255),
         ),
         textTheme:
-            const TextTheme(bodyLarge: TextStyle(fontFamily: 'Quicksand')),
+            const TextTheme(bodyLarge: TextStyle(fontFamily: 'Quicksand-Bold')),
       ),
       initialRoute: '/Login',
       routes: {
         '/': (context) => MapScreen(),
-        '/placeItinerary': (context) => PlaceItineraryPage(),
+        '/placeItinerary': (context) => const PlaceItineraryPage(),
         '/Login': (context) => LoginPage(),
         '/ForgotPassword': (context) => ForgotPasswordPage(),
         '/SignUp': (context) => SignUpPage(),
@@ -44,48 +48,54 @@ class MyApp extends StatelessWidget {
         '/itineraryPage': (context) => ItineraryPage(),
         '/profilePage': (context) => ProfilePage(),
         '/socialPage': (context) => SocialPage(),
+        '/accountPage': (context) => AccountPage(),
+        '/settingsPage': (context) => SettingsPage(),
         '/privacyPolicyPage': (context) => PrivacyPolicyPage(),
         '/notificationPage': (context) => NotificationsPage(),
-        '/settingsPage': (context) => SettingsPage(),
         '/sharedItinerariesPage': (context) => SharedItinerariesPage(),
-        '/accountPage': (context) => AccountPage()
+        '/socials1': (context) => Socials1(),
+        '/socials2': (context) => Socials2()
       },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  final PageStorageBucket bucket;
+
+  const MyHomePage({required this.bucket});
+
   @override
   MyHomePageState createState() => MyHomePageState();
 }
 
-class MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage>
+    with AutomaticKeepAliveClientMixin {
   int currentPageIndex = 0;
 
   @override
-  Widget build(BuildContext context) {
-    Widget currentPage;
-    switch (currentPageIndex) {
-      case 0:
-        currentPage = Container(
-          alignment: Alignment.center,
-          child: const Text('Page 1'),
-        );
-        break;
-      case 1:
-        currentPage = Container();
-        break;
-      case 2:
-        currentPage = Container();
-      case 3:
-        currentPage = Container();
-      default:
-        currentPage = Container();
-    }
+  bool get wantKeepAlive => true;
 
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
-      key: const PageStorageKey('HomePage'),
-      body: currentPage,
+      body: PageStorage(
+        key: const PageStorageKey('HomePage'),
+        bucket: widget.bucket,
+        child: IndexedStack(
+          index: currentPageIndex,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: const Text('Page 1'),
+            ),
+            Container(),
+            Container(),
+            Container(),
+          ],
+        ),
+      ),
       bottomNavigationBar: my_nav_bar.NavigationBar(
         selectedIndex: currentPageIndex,
         onDestinationSelected: (int index) {
